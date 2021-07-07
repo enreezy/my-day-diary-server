84ce7dc0aadd02ed869eccb45cc01dda79430b8f
Enrik Sabalvaro <sabalvaro.enrik@yahoo.com>
2021-07-08 00:20:39 +0800

    change port

--- server.js
+++ server.js
@@ -27,5 +27,5 @@ app.use("/", (req, res) => {
     res.send("My Day Diary");
 });
 
-const PORT = process.env.SERVER_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
